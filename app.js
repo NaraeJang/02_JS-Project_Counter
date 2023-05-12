7ca133b5eb587d7236eae1fd4d431d80a4f11fc2
NaraeJang <j.naaaarae@gmail.com>
2023-05-12 14:24:50 -0400

    Completed js

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ let count = 0;
 //select values
 const btns = document.querySelectorAll(".btn");
 const value = document.getElementById("value");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("reset")) {
+      count = 0;
+    } else if (styles.contains("increase")) {
+      count++;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count === 0) {
+      value.style.color = "#222";
+    } else if (count < 0) {
+      value.style.color = "red";
+    }
+
+    value.textContent = count;
+  });
+});
